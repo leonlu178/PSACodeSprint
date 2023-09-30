72732aa57907869f8caa7a930f3cb8f3e8b4bae4
Aron Andika <aron.andika11@gmail.com>
2023-09-30 21:37:16 +0800

    first

---
+++ bot.py
@@ -0,0 +1,18 @@
+import os
+import telebot
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+bot.infinity_polling()
